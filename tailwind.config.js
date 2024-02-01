1fe2a6501ad3bc9d1f5ceba8530f64221d062676
taki dilmi <angeldilmi@gmail.com>
2024-02-01 05:22:30 +0100

    secodn attempt to fix tailwindcss on deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', '.components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
